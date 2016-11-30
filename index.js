c8a1c4a171e437ce1f05ff4553a1a7e6eedd3e46
kokarn <kokarn@gmail.com>
2016-11-30 10:29:23 +0100

    Use data directly from the main repo instead of the local filesystem

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
+const https = require( 'https' );
 
 const Promise = require( 'promise' );
 
@@ -11,8 +12,41 @@ const reddit = require( './modules/reddit.js' );
 const STEAM_PAGES = 10;
 const REDDIT_PAGES = 10;
 
-const getGameData = function getGameData ( game ) {
-    return JSON.parse( fs.readFileSync( path.join( __dirname, `../dev-tracker/games/${ game }/data.json` ), 'utf8' ) );
+const GAME_LIST = [
+    'ark',
+    'battlefield1',
+    'csgo',
+    'elite',
+    'rainbow6',
+];
+
+const getGameData = function getGameData ( game, onDone ) {
+    const options = {
+        hostname: 'raw.githubusercontent.com',
+        method: 'GET',
+        path: `/kokarn/dev-tracker/master/games/${ game }/data.json`,
+    };
+
+    const request = https.request( options, ( response ) => {
+        let body = '';
+
+        response.setEncoding( 'utf8' );
+
+        response.on( 'data', ( chunk ) => {
+            body = body + chunk;
+        } );
+
+        response.on( 'end', () => {
+            onDone( JSON.parse( body ) );
+        } );
+    } );
+
+    request.on( 'error', ( requestError ) => {
+        // eslint-disable-next-line no-console
+        console.log( `problem with request: ${ requestError.message }` );
+    } );
+
+    request.end();
 }
 
 const getAccounts = function getAccounts ( developers, service, game ) {
@@ -29,14 +63,14 @@ const getAccounts = function getAccounts ( developers, service, game ) {
 };
 
 const findDevelopers = function findDevelopers ( game, gameIndex ) {
-    let gameData = getGameData( game );
-    let developers = gameData.developers;
+    getGameData( game, ( gameData ) => {
+        let developers = gameData.developers;
 
-    if ( gameData.config && gameData.config.Steam && gameData.config.Steam.matchOnly ) {
-        setTimeout( () => {
-            let steamDevelopers = getAccounts( developers, 'Steam', game );
+        if ( gameData.config && gameData.config.Steam && gameData.config.Steam.matchOnly ) {
+            setTimeout( () => {
+                let steamDevelopers = getAccounts( developers, 'Steam', game );
 
-            steam.get( gameData.config.Steam.matchOnly, STEAM_PAGES )
+                steam.get( gameData.config.Steam.matchOnly, STEAM_PAGES )
                 .then( ( users ) => {
                     let filteredUsers = steam.filter( users, game, steamDevelopers );
 
@@ -49,37 +83,36 @@ const findDevelopers = function findDevelopers ( game, gameIndex ) {
                 .catch( ( error ) => {
                     console.log( error );
                 } );
-        }, gameIndex * 10000 );
-    }
+            }, gameIndex * 10000 );
+        }
 
-    if ( gameData.config && gameData.config.Reddit && gameData.config.Reddit.index ) {
-        let redditDevelopers = getAccounts( developers, 'Reddit', game );
+        if ( gameData.config && gameData.config.Reddit && gameData.config.Reddit.index ) {
+            let redditDevelopers = getAccounts( developers, 'Reddit', game );
 
-        console.log( `Starting with page ${ gameData.config.Reddit.index }` );
-        reddit.get( gameData.config.Reddit.index, REDDIT_PAGES )
+            console.log( `Starting with page ${ gameData.config.Reddit.index }` );
+            reddit.get( gameData.config.Reddit.index, REDDIT_PAGES )
             .then( ( topUsers ) => {
                 reddit.get( `${ gameData.config.Reddit.index }/new`, REDDIT_PAGES )
-                    .then( ( newUsers ) => {
-                        let users = reddit.filter( topUsers.concat( newUsers ), game, redditDevelopers );
+                .then( ( newUsers ) => {
+                    let users = reddit.filter( topUsers.concat( newUsers ), game, redditDevelopers );
 
-                        console.log( `Found ${ users.length } new developers on Reddit for ${ game }` );
+                    console.log( `Found ${ users.length } new developers on Reddit for ${ game }` );
 
-                        if( users.length > 0 ) {
-                            console.log( JSON.stringify( users, null, 4 ) );
-                        }
-                    } )
-                    .catch( ( error ) => {
-                        console.log( error );
-                    } );
+                    if( users.length > 0 ) {
+                        console.log( JSON.stringify( users, null, 4 ) );
+                    }
+                } )
+                .catch( ( error ) => {
+                    console.log( error );
+                } );
             } )
             .catch( ( error ) => {
                 console.log( error );
             } );
-    }
+        }
+    } );
 }
 
-fs.readdir( path.join( __dirname, '../dev-tracker/games' ), ( error, files ) => {
-    for( let i = 0; i < files.length; i = i + 1 ) {
-        findDevelopers( files[ i ], i );
-    }
-} );
+for( let i = 0; i < GAME_LIST.length; i = i + 1 ) {
+    findDevelopers( GAME_LIST[ i ], i );
+}
