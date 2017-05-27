676e673b51fd024e4f335b0012ca2fb05fa3658c
Oskar Risberg <s24123@semcon.se>
2017-05-27 20:33:20 +0200

    Update flairs for Destiny

--- modules/flair.js
+++ modules/flair.js
@@ -495,6 +495,7 @@ module.exports = {
             'vanguardhonor',
             'vanguardhonoralt',
             'vanguardinsignia',
+            'vanguardquartermaster',
             'veterancrest',
             'vexlogo',
             'vextemplar',
