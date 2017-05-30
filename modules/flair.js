5a21ab34e2a5158cf4ba38efdef0410593e4387d
kokarn <kokarn@gmail.com>
2017-05-30 13:38:08 +0200

    Update flairs for Destiny

--- modules/flair.js
+++ modules/flair.js
@@ -358,6 +358,7 @@ module.exports = {
             'gorgon',
             'grimoirewriter',
             'gunslinger',
+            'gunsmith',
             'heartofthefoundation',
             'hexacon4',
             'hivelogo',
