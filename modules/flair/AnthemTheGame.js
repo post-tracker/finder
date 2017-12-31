a609e94200f3ac7e58fae8a08d046152d610359c
Oskar Risberg <oskar.risberg@stendahls.se>
2017-12-31 01:54:47 +0100

    Add anthem flairs

---
+++ modules/flair/AnthemTheGame.js
@@ -0,0 +1,22 @@
+module.exports = {
+    getFlairs: function getFlairs () {
+        return this.list;
+    },
+    list: [
+        'bacon bits on the surface of my mind',
+        'lpc',
+        'pc1',
+        'pc2',
+        'pc3',
+        'pc4',
+        'ps1',
+        'ps2',
+        'ps3',
+        'ps4',
+        'xb1',
+        'xb2',
+        'xb3',
+        'xb4',
+    ],
+    type: 'author_flair_css_class',
+};
