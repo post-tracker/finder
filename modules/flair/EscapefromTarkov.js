4d6559ff15d417416fa7ff623f98be101fa5d977
Oskar Risberg <oskar.risberg@stendahls.se>
2018-01-03 23:09:50 +0100

    Add EscapefromTarkov flairs

---
+++ modules/flair/EscapefromTarkov.js
@@ -0,0 +1,11 @@
+module.exports = {
+    getFlairs: function getFlairs () {
+        return this.list;
+    },
+    list: [
+        'flair-forummod',
+        'flair-mod',
+        'flair-twitch',
+    ],
+    type: 'author_flair_css_class',
+};
