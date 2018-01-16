86672d828ef04e2716f523a5b7607e778f99f9a8
Oskar Risberg <oskar.risberg@stendahls.se>
2018-01-16 18:14:39 +0100

    Flairs should really be lowercase

--- modules/flair/base.js
+++ modules/flair/base.js
@@ -4,7 +4,7 @@ module.exports = {
             return false;
         }
 
-        if ( this.list.includes( user[ this.type ] ) ) {
+        if ( this.list.includes( user[ this.type ].toLowerCase() ) ) {
             return false;
         }
 
