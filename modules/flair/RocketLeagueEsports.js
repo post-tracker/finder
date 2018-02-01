73411b03dc90dad65f9a4994f3d8ebccc534484a
Oskar Risberg <oskar.risberg@stendahls.se>
2018-02-01 10:14:14 +0100

    Add Rocket League

---
+++ modules/flair/RocketLeagueEsports.js
@@ -0,0 +1,42 @@
+const base = require( './base.js' );
+
+module.exports = Object.assign( {}, base, {
+    list: [
+        'allegiance',
+        'chiefs',
+        'cloud9',
+        'dignitas',
+        'envyus',
+        'flipside',
+        'fnatic',
+        'g2',
+        'galeforce',
+        'ghost',
+        'hollywoodhammers',
+        'ibuypower',
+        'kingsofurban',
+        'liquipedia',
+        'method',
+        'mld',
+        'mockit',
+        'mod',
+        'nexusgaming',
+        'northerngaming',
+        'nrg',
+        'octane',
+        'player', // Could re-add if we want to check for players
+        'psg',
+        'rivalesports',
+        'rivalseries', // Rocket League Rival Series
+        'rlc',
+        'rlcsstaff', // Rocket LEague Championship Series
+        'rogue',
+        'spl', // Shift pro league
+        'splyce',
+        'teamsecret',
+        'twitchstaff',
+        'verified', // Loads of random people
+        'veritas',
+    ],
+    type: 'author_flair_css_class',
+} );
