a818c90e310ee78e1c43f7ba7ce73a892e3540b4
Oskar Risberg <oskar.risberg@stendahls.se>
2018-05-29 09:08:56 +0200

    Switch to whitelist for fortnite

--- modules/flair/FortNiteBR.js
+++ modules/flair/FortNiteBR.js
@@ -1,7 +1,21 @@
-const fortnite = require( './FORTnITE.js' );
 const base = require( './base.js' );
 
 module.exports = Object.assign( {}, base, {
-    list: fortnite.list,
+    isDev: function isDev ( user ) {
+        if ( !user[ this.type ] ) {
+            return false;
+        }
+
+        for ( const flairString of this.list ) {
+            if ( user[ this.type ].includes( flairString ) ) {
+                return true;
+            }
+        }
+
+        return false;
+    },
+    list: [
+        'epic',
+    ],
     type: 'author_flair_css_class',
 } );
