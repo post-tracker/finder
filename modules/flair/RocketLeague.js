73c9732763c5079b4bedf527710cb6867e7a43e4
Oskar Risberg <oskar.risberg@stendahls.se>
2018-06-08 16:51:30 +0200

    Switch to whitelists for rocket league

--- modules/flair/RocketLeague.js
+++ modules/flair/RocketLeague.js
@@ -1,85 +1,21 @@
 const base = require( './base.js' );
 
 module.exports = Object.assign( {}, base, {
+    isDev: function isDev ( user ) {
+        if ( !user[ this.type ] ) {
+            return false;
+        }
+
+        for ( const flairString of this.list ) {
+            if ( user[ this.type ].includes( flairString ) ) {
+                return true;
+            }
+        }
+
+        return false;
+    },
     list: [
-        'alphac', // AlphaConsole
-        'aus',
-        'bronze1',
-        'bronze2',
-        'bronze3',
-        'can',
-        'chall',
-        'chall2',
-        'chall3',
-        'chall4',
-        'cham',
-        'cham1',
-        'cham2',
-        'cham3',
-        'diamond1',
-        'diamond2',
-        'diamond3',
-        'dutch',
-        'eng',
-        'fanalsyd', // Alpha Sydney
-        'fanc9',
-        'fanchief', // Fan Chiefs
-        'fancow',
-        'fanflip',
-        'fang2',
-        'fanibp',
-        'fankou',
-        'fanmeth',
-        'fanmock', // Fan Mock-It
-        'fannor',
-        'fannv',
-        'fanpock',
-        'fanpsg',
-        'fanrog',
-        'fansec', // Fan Team Secrecy
-        'fantsec', // Fan Team Secret
-        'flyquest',
-        'france',
-        'gale',
-        'gc',
-        'gccca', // College Carball Association
-        'gccow',
-        'gold1',
-        'gold2',
-        'gold3',
-        'grandegg',
-        'mld',
-        'mod2',
-        'nex',
-        'nor',
-        'plat1',
-        'plat2',
-        'plat3',
-        'prosp',
-        'prosp2',
-        'prosp4',
-        'psn',
-        'rlcd', // RLCD.gg
-        'rlcs', // Rocket League Championship Series
-        'rlesports', // /r/RocketLeagueEsports Moderator
-        'rlrs', // Maybe re-add in the future. Not a proper dev tho
-        'scot',
-        'silentecho',
-        'silver1',
-        'silver2',
-        'silver3',
-        'smashgg', // Maybe re-add in the future? No dev tho...
-        'star',
-        'star1',
-        'star2',
-        'star3',
-        'star4',
-        'steam',
-        'swe',
-        'twitch',
-        'unrank',
-        'usa',
-        'xbox',
+        'staff',
     ],
     type: 'author_flair_css_class',
 } );
--- modules/flair/RocketLeagueEsports.js
+++ modules/flair/RocketLeagueEsports.js
@@ -1,55 +1,21 @@
 const base = require( './base.js' );
 
 module.exports = Object.assign( {}, base, {
+    isDev: function isDev ( user ) {
+        if ( !user[ this.type ] ) {
+            return false;
+        }
+
+        for ( const flairString of this.list ) {
+            if ( user[ this.type ].includes( flairString ) ) {
+                return true;
+            }
+        }
+
+        return false;
+    },
     list: [
-        'allegiance',
-        'chiefs',
-        'clg',
-        'cloud9',
-        'complexity',
-        'dignitas',
-        'eg',
-        'envyus',
-        'eventvods', // Eventvods.com | Developer
-        'flipside',
-        'flyquest',
-        'fnatic',
-        'g2',
-        'galeforce',
-        'ghost',
-        'hollywoodhammers',
-        'ibuypower',
-        'kingsofurban',
-        'liquipedia',
-        'method',
-        'mld',
-        'mockit',
-        'mod',
-        'nexusgaming',
-        'none', // Special people, casters, reporters and stuff like that
-        'northerngaming',
-        'nrg',
-        'octane',
-        'player', // Could re-add if we want to check for players
-        'powerrankings', // Rocket League Power Rankings
-        'psg',
-        'rivalesports',
-        'rivalseries', // Rocket League Rival Series
-        'rlc',
-        'rlcs',  // Rocket League Championship Series
-        'rlcsstaff', // Rocket League Championship Series
-        'rlrs', // Rocket Leguer Rival Series
-        'rogue',
-        'silentecho',
-        'smashgg', // Smash.gg
-        'spl', // Shift pro league
-        'splyce',
-        'teamsecret',
-        'tripletrouble',
-        'twitchstaff',
-        'verified', // Loads of random people
-        'veritas',
-        'vitality',
+        'psyonixstaff',
     ],
     type: 'author_flair_css_class',
 } );
