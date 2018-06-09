0b2ccdc8d942682b0de75b0dea92aae1433213fc
Oskar Risberg <oskar.risberg@stendahls.se>
2018-06-09 12:41:40 +0200

    Add RealmRoyale flairs

---
+++ modules/flair/RealmRoyale.js
@@ -0,0 +1,9 @@
+const base = require( './base.js' );
+
+module.exports = Object.assign( {}, base, {
+    list: [
+        'chickenflair',
+        'mod',
+    ],
+    type: 'author_flair_css_class',
+} );
