b9b6b177d5ff43a393c152fd8b0ed518002e67f9
Oskar Risberg <oskar.risberg@stendahls.se>
2018-06-12 08:42:55 +0200

    Disable Discourse The endpoints we use are not generic

--- modules/finders/index.js
+++ modules/finders/index.js
@@ -6,5 +6,5 @@ module.exports = {
     Reddit: require( './Reddit.js' ),
     rsi: require( './rsi.js' ),
     Steam: require( './Steam.js' ),
-    Discourse: require( './Discourse.js' ),
+    // Discourse: require( './Discourse.js' ),
 };
