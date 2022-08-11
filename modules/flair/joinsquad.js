603f1394c6020cc0f38a5360664bca817bf34b14
Oskar Risberg <oskar.risberg@stendahls.se>
2022-08-11 09:43:25 +0200

    update flairs

---
+++ modules/flair/joinsquad.js
@@ -0,0 +1,21 @@
+const base = require( './base.js' );
+
+module.exports = Object.assign( {}, base, {
+    isDev: function isDev ( user ) {
+        if ( !user[ this.type ] ) {
+            return false;
+        }
+
+        for ( const flairString of this.list ) {
+            if ( user[ this.type ].includes( flairString ) ) {
+                return true;
+            }
+        }
+
+        return false;
+    },
+    list: [
+        'DeveloperFlair',
+    ],
+    type: 'author_flair_css_class',
+} );
