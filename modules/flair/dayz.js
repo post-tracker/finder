4b3a25c17f5452377aa56537a1c8c4c52847ce0e
Oskar Risberg <oskar.risberg@stendahls.se>
2026-06-13 08:50:10 +0200

    Don't guess DayZ moderators as developers
    Add the 'moderator' flair CSS class to the DayZ blocklist so users
    with that flair fall through isDev() as non-devs, alongside mod/user.

--- modules/flair/dayz.js
+++ modules/flair/dayz.js
@@ -3,6 +3,7 @@ const base = require( './base.js' );
 module.exports = Object.assign( {}, base, {
     list: [
         'mod',
+        'moderator',
         'user',
     ],
     type: 'author_flair_css_class',
